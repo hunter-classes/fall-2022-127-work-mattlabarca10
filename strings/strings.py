c54a53c998f4c17778e13021d605df50b17ef8e4
mattlabarca10 <104881180+mattlabarca10@users.noreply.github.com>
2022-09-15 04:40:06 +0000

    answers.py

---
+++ strings/strings.py
@@ -0,0 +1,15 @@
+s1 = "Hello World"
+s2 = 'another\nstring'
+s3 = """
+Multiline
+String
+"""
+s4 = s1+s1
+print(s4)
+print(s1*3)
+print(3*s1)
+
+print(len(s1))
+print(len("abcde"))
+
+# look for string functions
