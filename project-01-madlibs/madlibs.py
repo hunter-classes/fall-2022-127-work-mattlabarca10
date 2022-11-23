1166261035663b168a0f878076ec318159b01a9c
Matthew LaBarca <matlab0510@gmail.com>
2022-11-22 21:59:52 -0500

    project 2 complete no comments

--- project-01-madlibs/madlibs.py
+++ project-01-madlibs/madlibs.py
@@ -36,7 +36,7 @@ def madlibs(story):             # Thomas H helped me with the structure of this 
             if indName == -1:                     # if this is the first time name is used
                 new[ind] = randNounName()         # calls premade random function at index of <NOUNName> (repeated)
                 indName = ind                
-            else:                                 # else, use the name from the index of first use
+            else:                                 # else, use the name from the index of first use, so the character is consistent
                 new[ind] = new[indName]
             if "." in i:                          # if there was punctuation we add it back here (repeated)
                 new[ind] = new[ind] + "."
@@ -71,41 +71,3 @@ def madlibs(story):             # Thomas H helped me with the structure of this 
 
 print(madlibs(sread))
 
-
-
-
-
-
-# previously used stuff
-"""
-theName = randNounName()
-
-print("On a " +randAdj()+ " and " +randAdj()+ " day, " +theName+" "+randVerb()+ " a " +randAdj()+" "+
-randNounAnimal().lower()+". "+theName+ " did not regret it. In fact, "+theName+" did it again the next day. "
-+randAdj().capitalize()+" if you ask me. " +randAdj().capitalize()+" is how " +theName+
-" used to really be, but now he is just "+randAdj()+ ". I even remember when he "+randVerb()+
-" my brother. Unforgivable.")
-
-
-
-On a <ADJ> and <ADJ> day, <NOUNName> <PASTVERB> a <ADJ> <NOUNAnimal>. <NOUNName> did not regret it. In fact, 
-<NOUNName> did it again the next day. <ADJ> if you ask me. <NOUNName> used to be really <ADJ>, but now he 
-is just <ADJ>. I even remember when he <PASTVERB> my brother. Unforgivable.
-
-
-def randAdj():
-    adj = ["bright", "sunny", "gloomy", "dark", "stormy", "yellow", "scary", "plump", "juicy", "cool", "smart"]
-    return adj[random.randrange(len(adj))]
-
-def randNounName():
-    names = ["Matthew", "Muhammad", "Jose", "Derek", "Thomas", "Evan", "Godzilla", "LeBron"]
-    return names[random.randrange(len(names))]
-
-def randNounAnimal():
-    names = ["Monkey", "Dolphin", "Owl", "Wooly Mammoth", "Dinosaur", "Hawk", "Worm", "Python"]
-    return names[random.randrange(len(names))]
-
-def randVerb():
-    verb = ["ate", "threw", "saw", "killed", "married", "smacked", "licked", "tripped", "punted", "helped"]
-    return verb[random.randrange(len(verb))]
-"""
