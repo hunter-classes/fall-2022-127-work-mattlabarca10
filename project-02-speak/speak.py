6a48076def0c449cea84675fd2374aca4d1a10bf
Matthew LaBarca <matlab0510@gmail.com>
2022-11-27 21:13:53 -0500

    comments to speak.py

--- project-02-speak/speak.py
+++ project-02-speak/speak.py
@@ -1,15 +1,17 @@
+# Extras completed: pirate.dat, handles upper/lower cases, and advanced translations 
+# Sometimes
 pirate = open('pirate.dat').read()       # opens and reads all files    
 inp = open('input.txt').read()
-input = inp.lower().split()
-psplit = pirate.split('\n')
+input = inp.lower().split()              # each word in input is a list
+psplit = pirate.split('\n')              # each word in pirate is a list
 piratespeak = {}
-for word in psplit:
-    piratespeak.update({word[:word.index(":")]:word[word.index(':')+1:]})
+for word in psplit:                      # converting list of pirate into a dictionary
+    piratespeak.update({word[:word.index(":")]:word[word.index(':')+1:]})  
 keys = []
-for word in psplit:
+for word in psplit:                      # listing all the keys in dictionary into a list
     keys.append(word[:word.index(":")])
 npinput = []
-for word in input:
+for word in input:                       # making list of input with no punctuation to be put back later
     if ',' in word or '.' in word or '!' in word:
         npinput.append(word[:-1])
     else:
@@ -17,7 +19,7 @@ for word in input:
 
 newstory = []
 ind = 0
-for word in npinput:                                 
+for word in npinput:  # loops through npinput, replaces certain words/phrases to piratespeak using the dictionary
     if word in keys: 
         newstory.append(piratespeak[word])
     elif word == "<surprise>":
@@ -26,18 +28,17 @@ for word in npinput:
         newstory.append("Splice the mainbrace!")
     else:
         newstory.append(word)  
-    if ind == 0 or word == 'i' or '.' in input[ind-1] or '!' in input[ind-1]:
+    if ind == 0 or word == 'i' or '.' in input[ind-1] or '!' in input[ind-1]:  # properly re-capitalizing
         newstory[ind] = newstory[ind].capitalize() 
-    if "." in input[ind]:
+    if "." in input[ind]:               # adding back punctuation
         newstory[ind] = newstory[ind] + "."
     elif "," in input[ind]:
         newstory[ind] = newstory[ind] + ","
     elif "!" in input[ind]:
         newstory[ind] = newstory[ind] + "!"
     ind+=1
-final = ' '.join(newstory)
+final = ' '.join(newstory)              # making final story into one string
 
-print(input)
 print(final)
 
 
