545b1cdac73750bffbc679bc834980f0e730f5e5
Matthew LaBarca <matlab0510@gmail.com>
2022-12-05 23:15:06 -0500

    c++ project pretty much done, need comments still

---
+++ cpp/main.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+int largest(int arr[], int n){
+    int max = arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] > max)
+            max = arr[i];
+    return max;
+}
+
+int doub(int a){
+    return a*2;
+}
+
+
+int main()
+{
+    int a = 1;
+    int b = 5;
+    int c = 9;
+
+    cout<<"Right now, a = "<<a<<", b = "<<b<<", and c = "<<c<<"."<<endl;
+    b = doub(b);
+    cout<<"The variable b has now been doubled."<<endl;
+    int array[] = {a,doub(b),c};
+    cout<<"Out of these numbers, the largest number is "<<largest(array,3)<<" since b was doubled."<<endl;
+    return 0;
+}
+
+
+/*
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+    int max(int a, int b);
+    {
+       if(a > b){
+            return a;
+        }
+        else{
+            return b;
+        }
+        }
+        return a > b;
+    }
+    std::cout << max(4,6) << std::endl;
+    return 0;
+}
+*/
